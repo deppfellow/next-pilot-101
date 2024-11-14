e5fce1e57bf57a5e5189de587ddf4bfbc167c563
raidrafif <raid.rafif@sigma.co.id>
2024-11-14 16:51:39 +0700

    chore: Initial commit

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+function DashboardLayout({ children }: Props) {
+  return (
+    <>
+      <main>DashboardLayout</main>
+    </>
+  );
+}
+
+export default DashboardLayout;
